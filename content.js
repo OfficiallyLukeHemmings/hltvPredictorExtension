bb051ff121a2ad2a701c5dc91bb4e033a2562917
Luke - PC <officiallylukehemmings@gmail.com>
2022-04-23 02:46:09 +0100

    Minimal gameData scraping
    TODO: Fetch to API
    TODO: Display window (modal?) for game score / recommendation?

--- content.js
+++ content.js
@@ -1,17 +1,23 @@
-// 
-const countdown = document.querySelector("body > div.bgPadding > div.widthControl > div.colCon > div.contentCol > div.match-page > div.standard-box.teamsBox > div.timeAndEvent > div.countdown").innerText;
+// Coutdown innerText
+let countdown = document.querySelector("body > div.bgPadding > div.widthControl > div.colCon > div.contentCol > div.match-page > div.standard-box.teamsBox > div.timeAndEvent > div.countdown").innerText;
+if (countdown == null) {
+    // Try for a LIVE game instead
+    countdown = document.querySelector("body > div.bgPadding > div.widthControl > div.colCon > div.contentCol > div.match-page > div.standard-box.teamsBox > div.timeAndEvent > div.countdown.countdown-live")
+}
 
-// If match is not complete...
+// Get features from upcoming/ongoing game to fetch to API for result.
 if (countdown != "Match over") {
-    // Get features here to fetch to API for result
-    const bettingOddsDiff = getBettingOdds()
+    
+    window.addEventListener("load", (e => {
+        // Ensuring the DOM is loaded before seeking the data
+        const gameDetails = getGameDetails();
+    }))
 }
 
 
-function getBettingOdds() {
-    // Testing getting betting odds from extension
+function getBettingDiff() {
+    // Testing getting betting odds difference from extension
     const bettingTable = document.querySelector("#betting > div.world-vVP3JBh5AswY4yBD-list > div > div.three-quarter-width > div > table > tbody");
-
     const rowsArray = Array.from(bettingTable.rows);
 
     // Removing title row
@@ -39,5 +45,39 @@ function getBettingOdds() {
         }
     });
 
-    console.log('Combined Betting Odds Difference:', bettingDifferencesSums/validBettingRows);    
+    // Return the mean betting difference (absolute value) to 3dp.
+    return (bettingDifferencesSums / validBettingRows).toFixed(3)
+}
+
+
+function getTeamRanks() {
+    const teamRanks = {
+        team1Rank: null,
+        team2Rank: null
+    };
+
+    rankingDivs = document.getElementsByClassName("teamRanking");
+    teamRanks.team1Rank = parseInt(rankingDivs[0].firstElementChild.lastChild.data.substring(1));
+    teamRanks.team2Rank = parseInt(rankingDivs[1].firstElementChild.lastChild.data.substring(1));    
+
+    return teamRanks;
+}
+
+
+function getGameDetails() {
+    // Creating gameDetails object to be sent to ML API
+    const gameDetails = {
+        eventType: null,
+        team1Rank: null,
+        team2Rank: null,
+        bettingOddsDiff: null
+    };
+
+    gameDetails.bettingOddsDiff = getBettingDiff();
+    const teamRanks = getTeamRanks();
+    gameDetails.team1Rank = teamRanks.team1Rank;
+    gameDetails.team2Rank = teamRanks.team2Rank;
+
+    console.log(gameDetails);
+    return gameDetails;
 }
